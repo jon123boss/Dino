60d52583992e82a04799990a3fbbffeeed5838a3
Jonnester <84779222+jon123boss@users.noreply.github.com>
2024-08-05 13:53:37 +0800

    Cactus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -27,13 +28,40 @@ dino_vel_y = 0
 
 ground_rect = pygame.Rect(0, SCREEN_HEIGHT - 50, SCREEN_WIDTH, 50)
 
+obstacle_width = 20
+obstacle_height = 40
+obstacle_min_gap = 200
+obstacle_max_gap = 400
+obstacle_speed = 5
+obstacle_list = []
+
+class Obstacle:
+    def __init__(self, x):
+        self.x = x
+        self.y = SCREEN_HEIGHT - obstacle_height - 50
+        self.width = obstacle_width
+        self.height = obstacle_height
+
+    def move(self):
+        self.x -= obstacle_speed
+
+    def draw(self):
+        pygame.draw.rect(screen, BLACK, (self.x, self.y, self.width, self.height))
+
+    def off_screen(self):
+        return self.x < -self.width
+
 def draw_ground():
     pygame.draw.rect(screen, GROUND_COLOR, ground_rect)
 
 def draw_dinosaur(x, y):
     pygame.draw.rect(screen, BLACK, (x, y, dino_width, dino_height))
 
+def check_collision(dino_rect, obstacle_rect):
+    return dino_rect.colliderect(obstacle_rect)
+
 running = True
+obstacle_timer = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,9 +81,28 @@ while running:
             dino_jump = False
             dino_vel_y = 0
 
+    if obstacle_timer == 0:
+        obstacle_list.append(Obstacle(SCREEN_WIDTH))
+        obstacle_timer = random.randint(obstacle_min_gap, obstacle_max_gap)
+    else:
+        obstacle_timer -= 1
+
+    for obstacle in obstacle_list[:]:
+        obstacle.move()
+        if obstacle.off_screen():
+            obstacle_list.remove(obstacle)
+
+    dino_rect = pygame.Rect(dino_x, dino_y, dino_width, dino_height)
+    for obstacle in obstacle_list:
+        obstacle_rect = pygame.Rect(obstacle.x, obstacle.y, obstacle.width, obstacle.height)
+        if check_collision(dino_rect, obstacle_rect):
+            running = False
+
     screen.fill(WHITE)
     draw_ground()
     draw_dinosaur(dino_x, dino_y)
+    for obstacle in obstacle_list:
+        obstacle.draw()
 
     pygame.display.flip()
 
